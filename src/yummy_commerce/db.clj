accfd37c19af7e96400eaa45b7ae1938bf587a4f
Leo Crapart <leocrapart@yahoo.fr>
2022-06-17 23:59:50 +0200

    db definition

---
+++ src/yummy_commerce/db.clj
@@ -0,0 +1,15 @@
+(ns db)
+
+
+
+(defn get-product-by-id [id]
+	({:id "id"
+		:name "name"}))
+
+
+(defn create-product [product])
+
+(defn delete-product [product-id])
+
+(defn update-product [product])
+
