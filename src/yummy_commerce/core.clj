ea85eb0a9f3bc3344242347f513a6c8b7f26fb9e
leo crapart <leocrapart@yahoo.fr>
2022-06-24 17:30:45 +0200

    uberjar

--- src/yummy_commerce/core.clj
+++ src/yummy_commerce/core.clj
@@ -3,125 +3,8 @@
   (:require [org.httpkit.server :as server]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [clj-postgresql.core :as pg]
-            [clojure.java.jdbc :as jdbc]
             [clojure.data.json :as json]
             [yummy-commerce.mongodb :as db]))
-
-
-; (defn open-db []
-;   (pg/pool :host "dumbo.db.elephantsql.com" 
-;            :user "dqgqrqev" 
-;            :dbname "dqgqrqev" 
-;            :password "rtDGlzTFU7lzeZHrMdQTMIZiFMjr4unD"))
-
-; (defn single-query [query-string]
-;   (let [db (open-db)
-;         query-result (jdbc/query db [query-string])
-;         close-db (pg/close! db)]
-;     query-result))
-
-; ;; get
-
-; (defn get-product [id]
-;   (let [query-result (single-query (str "SELECT * FROM products WHERE id =" id))
-;         first-element (first query-result)]
-;     first-element))
-
-
-; ;; post 
-
-; (defn insert-product [product]
-;   (single-query (str "INSERT INTO products(id, name, price_230, price_340, quantity, description) 
-;     VALUES(" (product :id) "," (product :name) "," (product :price_230) "," (product :price_340) "," (product :quantity) "," (product :description) ");")))
-
-
-; (defn insert-product-magie []
-;   (insert-product
-;     {:id "'magie2'"
-;      :name "''"
-;      :price_230 4
-;      :price_340 5
-;      :quantity 0
-;      :description "'super description2'"}))
-
-; (insert-product-magie)
-
-
-; (def fraise (get-product "'fraise'"))
-; (def cerise (get-product "'cerise'"))
-; cerise
-; fraise
-
-
-
-; (defn fps-handler [req]
-;   {:status 200
-;    :headers {"Content-Type" "text/html"}
-;    :body "Pew pew!"})
-
-
-; (defn fraise []
-;   (get-product "'fraise'"))
-
-; (defn fraise-str []
-;   (json/write-str (fraise)))
-
-; (defn fraise-handler [req]
-;   {:status 200
-;    :headers {"Content-Type" "text/json"}
-;    :body (fraise-str)})
-
-
-; (defn product [id]
-;   (get-product (str "'" id "'")))
- 
-; (defn product-str [id]
-;   (json/write-str (product id)))
-
-; (product-str "coing-vanille")
-
-
-; (defn product-not-found-str [product-id]
-;   (json/write-str {:error (str "Product '" product-id "' not found in database.")}))
-
-; (defn product-not-found-res [product-id]
-;   {:status 404
-;    :headers {"Content-Type" "text/json"}
-;    :body (product-not-found-str product-id)})
-
-; (defn product-found-res [product-string]
-;   {:status 200
-;    :headers {"Content-Type" "text/json"}
-;    :body product-string})
-
-; (defn product-res [req]
-;   (let [product-id ((req :params) :product-id)
-;         product-string (product-str product-id)]
-;     (if (= product-string "null")
-;       (product-not-found-res product-id)
-;       (product-found-res product-string))))
-      
-
-; (defn mail-man []
-;   "{\"Spongebob Narrator\": \"5 years later...\"}")
-
-; (defn mail-handler [req]
-;   {:status 200
-;    :headers {"Content-Type" "text/json"}
-;    :body (mail-man)})
-
-; (defn general-handler [req]
-;   {:status 200
-;    :headers {"Content-Type" "text/html"}
-;    :body "All hail General Zod!"})
-
-; (defroutes app-routes
-;   (GET "/" [] fps-handler)
-;   (GET "/:product-id" [] product-res)
-;   (POST "/postoffice" [] mail-handler)
-;   (ANY "/anything-goes" [] general-handler)
-;   (route/not-found "You Must Be New Here"))
 
 
 (def html-header
@@ -131,97 +14,7 @@
   {"Content-Type" "text/json"})
 
 
-
-;; get
-
-(defn get-product [product-id]
-  {:id product-id
-   :name "product-name"
-   :description "description"
-   :price "price"})
-
-(defn product-found-res [product]
-  {:status 200
-   :headers json-header
-   :body (json/write-str product)})
-
-(defn product-not-found-res [product-id]
-  {:status 404
-   :headers json-header
-   :body (json/write-str {:msg (str "product " product-id " not found")})})
-  
-(defn get-product-res [req]
-  (let [product-id ((req :params) :product-id)
-        product (get-product product-id)]
-    (if product
-      (product-found-res product)
-      (product-not-found-res product-id))))
-      
-; (if (get-product "fraise")
-;   true
-;   false)
-
-
-
-;; create
-
-(defn create-product [product-id product]
-  {:id product-id
-   :name "product-name"
-   :description "description"
-   :price "price"})
-
-(defn create-product-res [req]
-  {:status 201
-   :headers json-header
-   :body (json/write-str (create-product "fraise"))})
-
-(defn created-product-res [product]
-  {:status 201
-   :headers json-header
-   :body (json/write-str product)})
-
-(def not-created-product-res
-  {:status 405})
-
-(defn create-product-res [req]
-  (let [product-id ((req :params) :product-id)
-        payload "payload"
-        created-product (create-product product-id payload)]
-    (if created-product
-      (created-product-res created-product)
-      not-created-product-res)))
-
-
-;; update
-
-(defn update-product [product-id]
-  {:id product-id
-   :name "product-name"
-   :description "description"
-   :price "price"})
-
-(defn update-product-res [req]
-  {:status 200
-   :headers json-header
-   :body (json/write-str (update-product "fraise"))})
-
-;; delete
-
-(defn delete-product-res [req]
-  {:status 200})
-
-(defn get-all-products-res [req]
-  req)
-
-(defn delete-all-products-res [req]
-  req)
-
-(defn get-season-products-res [req]
-  req)
-
-(defn process-payment-res [req]
-  req)
+;;
 
 
 (defn get-confiture-res [req]
@@ -239,41 +32,22 @@
      :body (json/write-str sucette)}))
 
 (defn get-confitures-res [req]
-  (let [confitures (db/confitures)]
-    {:status 200
-     :headers json-header
-     :body (json/write-str confitures)}))
-
-(clojure.string/split "season=ete" #"=")
-
-
+  {:status 200
+   :headers json-header
+   :body (json/write-str (db/confitures))})
 
 (defn get-sucettes-res [req]
-  (let [query-string (req :query-string)
-        season (nth (clojure.string/split query-string #"=") 1)
-        req2 (dissoc req :async-channel)]
-    {:status 200
-     :headers json-header
-     :body (json/write-str req2)}))
+  {:status 200
+   :headers json-header
+   :body (json/write-str (db/sucettes))})
+
 
 
 (defroutes api-routes
   (GET "/confitures/:id" [] get-confiture-res)
   (GET "/sucettes/:id" [] get-sucette-res)
   (GET "/confitures" [] get-confitures-res)
-  (GET "/sucettes" {params :params} [] get-sucettes-res)
-
-
-
-
-  (GET "/product/:product-id" [] get-product-res)
-  (POST "/product/:product-id" [] create-product-res)
-  (PUT "/product/:product-id" [] update-product-res)
-  (DELETE "/product/:product-id" [] delete-product-res)
-  (DELETE "/products" [] delete-all-products-res)
-  (GET "/products/:season" [] get-season-products-res)
-  (POST "/payment" [] process-payment-res))
-  
+  (GET "/sucettes" [] get-sucettes-res))
 
 
 (defn -main
@@ -284,6 +58,6 @@
     (println (str "Running webserver at http://127.0.0.1:" port "/"))))
 
 ;;launch server from repl
-(-main)
+; (-main)
 
 
