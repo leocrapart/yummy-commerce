78fcf278b9377fcd02050f5e1c4f4f8f88d8752c
leo crapart <leocrapart@yahoo.fr>
2022-06-24 17:59:47 +0200

    params parsing

--- src/yummy_commerce/core.clj
+++ src/yummy_commerce/core.clj
@@ -31,10 +31,35 @@
      :headers json-header
      :body (json/write-str sucette)}))
 
+;; query string decode
+;; recur take key,=, take value
+;; parse-value
+;; key to keyword
+;;return param map
+
+;; id=2&name=fraise&camion=tchoutchou
+;; => {:id "2" :name "fraise" :camion "tcoutchou"}
+
+(defn keywordized-vector [vector]
+  [(keyword (nth vector 0)) (nth vector 1)])
+
+
+(keywordized-vector ["id" "2"])
+
+(defn params [query-string]
+  (into {}
+    (map keywordized-vector
+      (map (fn [el] (clojure.string/split el #"="))
+        (clojure.string/split "id=2&name=fraise&camion=tchoutchou" #"&")))))
+
+; (params "id=2&name=fraise&camion=tchoutchou")
+
 (defn get-confitures-res [req]
-  {:status 200
-   :headers json-header
-   :body (json/write-str (db/confitures))})
+  (let [query-params (params (req :query-string))
+        season (query-params :season)]
+    {:status 200
+     :headers json-header
+     :body (json/write-str query-params)}))
 
 (defn get-sucettes-res [req]
   {:status 200
@@ -46,8 +71,9 @@
 (defroutes api-routes
   (GET "/confitures/:id" [] get-confiture-res)
   (GET "/sucettes/:id" [] get-sucette-res)
-  (GET "/confitures" [] get-confitures-res)
+  (GET "/confitures" get-confitures-res)
   (GET "/sucettes" [] get-sucettes-res))
+  
 
 
 (defn -main
@@ -58,6 +84,6 @@
     (println (str "Running webserver at http://127.0.0.1:" port "/"))))
 
 ;;launch server from repl
-; (-main)
+(-main)
 
 
